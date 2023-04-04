8071d5ea3f165ec967cc8cf4c0d3a2b81a5a51b6
Kleyton Nascimento <kleytonstn@gmail.com>
2023-04-04 10:22:02 -0300

    add database connection

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+try {
+  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+  console.log('Connected to the database');
+} catch (error) {
+  console.log('Could not connect to the database. Exiting now...', error);
+}
 
 let Person;
 
